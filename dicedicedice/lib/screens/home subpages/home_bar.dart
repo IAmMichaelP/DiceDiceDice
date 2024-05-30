8c84e98998c5323dc8475790b824ef9418c97d09
Kristan Jay Gabay <ktgabay@up.edu.ph>
2024-05-30 13:50:35 +0800

    fixed bugs

--- dicedicedice/lib/screens/home subpages/home_bar.dart
+++ dicedicedice/lib/screens/home subpages/home_bar.dart
@@ -35,6 +35,11 @@ class _HomeBarState extends State<HomeBar> {
   int diceResult = 0;
   int diceMax = 20;
   String diceType = 'd20';
+  List<String> currentDiceImages = [];
+  String question = '';
+  String interpretation = '';
+  Timestamp timeStamp = Timestamp.now();
+  String dice = '';
 
   final List<String> d20_images = [
     'assets/d20/1d20.png',
@@ -116,13 +121,8 @@ class _HomeBarState extends State<HomeBar> {
     'assets/d12/12d12.png',
     'assets/d20/20d20.png',
   ];
-  List<String> currentDiceImages = [];
-  final AudioPlayer player = AudioPlayer();
 
-  String question = '';
-  String interpretation = '';
-  Timestamp timeStamp = Timestamp.now();
-  String dice = '';
+  final AudioPlayer player = AudioPlayer();
 
   @override
   void initState() {
@@ -254,132 +254,129 @@ class _HomeBarState extends State<HomeBar> {
       }
     }
 
-    return SingleChildScrollView(
-      child: Center(
+    return Center(
         child: Column(
-          children: [
-            const SizedBox(height: 30),
-            Image.asset('assets/Dice-cider.png'),
-            const SizedBox(height: 10),
-            Image.asset('assets/subtitle.png'),
-            const SizedBox(height: 10),
-            Transform.rotate(
-              angle: random.nextDouble() * 180,
-              child: Image.asset(
-                currentDiceImages[currentImageIndex],
-                height: 100,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      maxLines: 4,
-                      decoration: InputDecoration(
-                        constraints: BoxConstraints(
-                            maxWidth: MediaQuery.of(context).size.width * 0.85),
-                        filled: true,
-                        fillColor: Color(0xFF8C5E33), // Background color
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          borderSide:
-                              BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          borderSide: BorderSide(
-                              color: Color.fromARGB(255, 143, 94,
-                                  11)), // Border radius when focused
-                        ),
-                        hintText: 'Enter your indecisiveness here',
-                        hintStyle: const TextStyle(
-                            color: Color.fromARGB(255, 55, 44, 44),
-                            fontFamily: 'Brawler'),
-                      ),
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Brawler'),
-                      onChanged: (value) {
-                        question = value;
-                      },
-                      validator: (value) {
-                        if (value?.trim().isEmpty ?? false) {
-                          return "Please provide indecisiveness";
-                        }
-                        return null;
-                      }, // Text color
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () async {
-                        rollDice();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize:
-                            Size(MediaQuery.of(context).size.width * 0.7, 55),
-                        side: const BorderSide(
-                            color: Color(0xFF8C5E33), width: 2.0),
-                        backgroundColor: Color(0xFF161312), // Button color
-                        foregroundColor: Color(0xFF8C5E33), // Text color
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Roll A Dice',
-                          style: TextStyle(
-                              fontFamily: 'Brawler',
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Wrap(
-                      alignment: WrapAlignment.center,
-                      children: dice_all.map((diceImage) {
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              if (diceImage == 'assets/d4/4d4.png') {
-                                diceType = 'd4';
-                              } else if (diceImage == 'assets/d6/6d6.png') {
-                                diceType = 'd6';
-                              } else if (diceImage == 'assets/d8/8d8.png') {
-                                diceType = 'd8';
-                              } else if (diceImage == 'assets/d10/9d10.png') {
-                                diceType = 'd10';
-                              } else if (diceImage == 'assets/d12/12d12.png') {
-                                diceType = 'd12';
-                              } else if (diceImage == 'assets/d20/20d20.png') {
-                                diceType = 'd20';
-                              }
-                              currentImageIndex = dice_all.indexOf(diceImage);
-                              updateDiceImages(currentImageIndex);
-                            });
-                          },
-                          child: Container(
-                            height: 75,
-                            decoration: const BoxDecoration(
-                                // color: Colors.blue,
-                                // borderRadius: BorderRadius.circular(8.0),
-                                ),
-                            padding: EdgeInsets.all(8.0),
-                            child: Image.asset(
-                              diceImage,
-                              height: 50,
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    ),
-                  ],
-                ))
-          ],
+      children: [
+        const SizedBox(height: 30),
+        Image.asset('assets/Dice-cider.png'),
+        const SizedBox(height: 10),
+        Image.asset('assets/subtitle.png'),
+        const SizedBox(height: 10),
+        Transform.rotate(
+          angle: random.nextDouble() * 180,
+          child: Image.asset(
+            currentDiceImages[currentImageIndex],
+            height: 100,
+          ),
         ),
-      ),
-    );
+        const SizedBox(height: 10),
+        Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.85),
+                    filled: true,
+                    fillColor: Color(0xFF8C5E33), // Background color
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(
+                              255, 143, 94, 11)), // Border radius when focused
+                    ),
+                    hintText: 'Enter your indecisiveness here',
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 55, 44, 44),
+                        fontFamily: 'Brawler'),
+                  ),
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Brawler'),
+                  onChanged: (value) {
+                    question = value;
+                  },
+                  validator: (value) {
+                    if (value?.trim().isEmpty ?? false) {
+                      return "Please provide indecisiveness";
+                    }
+                    return null;
+                  }, // Text color
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    rollDice();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize:
+                        Size(MediaQuery.of(context).size.width * 0.7, 55),
+                    side:
+                        const BorderSide(color: Color(0xFF8C5E33), width: 2.0),
+                    backgroundColor: Color(0xFF161312), // Button color
+                    foregroundColor: Color(0xFF8C5E33), // Text color
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Roll A Dice',
+                      style: TextStyle(
+                          fontFamily: 'Brawler',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  children: dice_all.map((diceImage) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          if (diceImage == 'assets/d4/4d4.png') {
+                            diceType = 'd4';
+                          } else if (diceImage == 'assets/d6/6d6.png') {
+                            diceType = 'd6';
+                          } else if (diceImage == 'assets/d8/8d8.png') {
+                            diceType = 'd8';
+                          } else if (diceImage == 'assets/d10/9d10.png') {
+                            diceType = 'd10';
+                          } else if (diceImage == 'assets/d12/12d12.png') {
+                            diceType = 'd12';
+                          } else if (diceImage == 'assets/d20/20d20.png') {
+                            diceType = 'd20';
+                          }
+                          currentImageIndex = dice_all.indexOf(diceImage);
+                          updateDiceImages(currentImageIndex);
+                        });
+                      },
+                      child: Container(
+                        height: 75,
+                        decoration: const BoxDecoration(
+                            // color: Colors.blue,
+                            // borderRadius: BorderRadius.circular(8.0),
+                            ),
+                        padding: EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          diceImage,
+                          height: 50,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ))
+      ],
+    ));
   }
 }
--- dicedicedice/lib/screens/home_screen.dart
+++ dicedicedice/lib/screens/home_screen.dart
@@ -1,19 +1,13 @@
 import 'dart:async';
-
+import 'package:dicedicedice/screens/home%20subpages/history_bar.dart';
+import 'package:dicedicedice/screens/home%20subpages/home_bar.dart';
 import 'package:dicedicedice/screens/landing_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import '../service/auth_service.dart';
-import 'dart:math';
-import 'package:flutter/scheduler.dart' show timeDilation;
-import 'package:just_audio/just_audio.dart';
-import '../service/database_service.dart';
-import '../model/user_model.dart';
-import 'home subpages/history_bar.dart';
-import 'home subpages/home_bar.dart';
 import 'about_screen.dart';
-// import 'landing_screen.dart';
+import '../model/user_model.dart';
+import '../service/database_service.dart';
 
 class HomeScreen extends StatelessWidget {
   final String uid;
@@ -26,8 +20,6 @@ class HomeScreen extends StatelessWidget {
     final String? uid = authService.uid;
     final databaseService = DatabaseService(uid: uid);
 
-    timeDilation = 5.0;
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -37,18 +29,24 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: const Color(0xFF181415),
           automaticallyImplyLeading: false,
           title: StreamBuilder<UserModel>(
-              stream: databaseService.userData,
-              builder: (context, snapshot) {
-                UserModel? userData = snapshot.data;
+            stream: databaseService.userData,
+            builder: (context, snapshot) {
+              UserModel? userData = snapshot.data;
 
-                return Text(
-                  userData!.username,
-                  style: const TextStyle(
-                      fontSize: 30,
-                      fontFamily: 'NewRocker',
-                      color: Color(0xFFF6D5B7)),
-                );
-              }),
+              if (userData == null) {
+                return CircularProgressIndicator();
+              }
+
+              return Text(
+                userData.username,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'NewRocker',
+                  color: Color(0xFFF6D5B7),
+                ),
+              );
+            },
+          ),
           bottom: const TabBar(
             labelColor: Color(0xFFF6D5B7),
             indicatorColor: Color(0xFFF6D5B7),
@@ -59,8 +57,10 @@ class HomeScreen extends StatelessWidget {
           ),
           actions: [
             PopupMenuButton<String>(
-              icon: const Icon(Icons.more_vert,
-                  color: Color.fromARGB(255, 254, 214, 154)),
+              icon: const Icon(
+                Icons.more_vert,
+                color: Color.fromARGB(255, 254, 214, 154),
+              ),
               color: const Color.fromRGBO(38, 50, 20, 1),
               onSelected: (String value) {
                 if (value == 'about') {
@@ -69,24 +69,22 @@ class HomeScreen extends StatelessWidget {
                     MaterialPageRoute(builder: (context) => AboutScreen()),
                   );
                 } else if (value == 'logout') {
-                  //   authService.signOut();// sign out
-                  //   Navigator.pushAndRemoveUntil(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => WelcomeScreen()),
-                  //   (Route<dynamic> route) => false,
-                  // );
+                  authService.signOut();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => WelcomeScreen()),
+                    (Route<dynamic> route) => false,
+                  );
                 }
               },
               itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
                 const PopupMenuItem<String>(
                   value: 'about',
                   child: ListTile(
-                    // tileColor: const Color.fromRGBO(38, 50, 20, 1),
-                    // shape: RoundedRectangleBorder(
-                    // borderRadius: BorderRadius.circular(0),
-                    // side: const BorderSide(color: Color.fromRGBO(255, 198, 181, 181)), ),
-                    leading: Icon(Icons.info,
-                        color: Color.fromARGB(255, 248, 227, 197)),
+                    leading: Icon(
+                      Icons.info,
+                      color: Color.fromARGB(255, 248, 227, 197),
+                    ),
                     title: Text(
                       'About',
                       style: TextStyle(
@@ -98,20 +96,22 @@ class HomeScreen extends StatelessWidget {
                 PopupMenuItem<String>(
                   value: 'logout',
                   child: ListTile(
-                    leading: Icon(Icons.logout,
-                        color: Color.fromARGB(255, 248, 227, 197)),
+                    leading: Icon(
+                      Icons.logout,
+                      color: Color.fromARGB(255, 248, 227, 197),
+                    ),
                     title: Text(
                       'Log Out',
-                      style:
-                          TextStyle(color: Color.fromARGB(255, 198, 181, 181)),
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 198, 181, 181),
+                      ),
                     ),
                     onTap: () {
-                      authService.signOut(); // Sign out
+                      authService.signOut();
                       Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                WelcomeScreen()), // Navigate to the login or welcome screen
+                            builder: (context) => WelcomeScreen()),
                         (Route<dynamic> route) => false,
                       );
                     },
@@ -121,144 +121,38 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
-
         body: Column(
           children: [
             Expanded(
               child: Stack(
                 children: [
-                  // Background image
-
                   Positioned.fill(
-                      child: Stack(children: [
-                    Image.asset(
-                      "assets/img1.png",
-                      fit: BoxFit.cover,
-                      width: double.infinity,
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          "assets/img1.png",
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ],
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      color: Colors.black.withOpacity(0.5), // Dark overlay
-                    ),
-                  ])),
-
-                  Expanded(
-                    child: TabBarView(children: <Widget>[
+                  ),
+                  TabBarView(
+                    children: <Widget>[
                       HomeBar(uid: uid!),
-                      HistoryBar(uid: uid!)
-                    ]),
+                      HistoryBar(uid: uid!),
+                    ],
                   ),
                 ],
               ),
             ),
           ],
         ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: () => Navigator.push(
-        //     context,
-        //     MaterialPageRoute(builder: (context) => const MyWidget2()),
-        //   ),
-        //   child: const Icon(Icons.add),
-        // ),
       ),
     );
   }
 }
-
-// class MyWidget2 extends StatefulWidget {
-//   const MyWidget2({super.key});
-
-//   @override
-//   _MyWidget2State createState() => _MyWidget2State();
-// }
-
-// class _MyWidget2State extends State<MyWidget2> {
-//   final Random random = Random();
-//   int currentImageIndex = 0;
-//   int counter = 1;
-//   final List<String> images = [
-//     'd20/1d20.png',
-//     'd20/2d20.png',
-//     'd20/3d20.png',
-//     'd20/4d20.png',
-//     'd20/5d20.png',
-//     'd20/6d20.png',
-//     'd20/7d20.png',
-//     'd20/8d20.png',
-//     'd20/9d20.png',
-//     'd20/10d20.png',
-//     'd20/11d20.png',
-//     'd20/12d20.png',
-//     'd20/13d20.png',
-//     'd20/14d20.png',
-//     'd20/15d20.png',
-//     'd20/16d20.png',
-//     'd20/17d20.png',
-//     'd20/18d20.png',
-//     'd20/19d20.png',
-//     'd20/20d20.png',
-//   ];
-//   final AudioPlayer player = AudioPlayer();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     timeDilation = 5.0;
-
-//     return Scaffold(
-//       body: Align(
-//         alignment: Alignment.topCenter,
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             Transform.rotate(
-//               angle: random.nextDouble() * 180,
-//               child: Image.asset(
-//                 images[currentImageIndex],
-//                 height: 100,
-//               ),
-//             ),
-//             const SizedBox(height: 60),
-//             ElevatedButton(
-//               onPressed: () async {
-//                 // Sound
-
-//                 // Roll the dice
-//                 Timer.periodic(const Duration(milliseconds: 80), (timer) {
-//                   counter++;
-//                   setState(() {
-//                     currentImageIndex = random.nextInt(20);
-//                   });
-
-//                   if (counter >= 13) {
-//                     timer.cancel();
-
-//                     setState(() {
-//                       counter = 1;
-//                     });
-//                   }
-//                 });
-//               },
-//               child: const Padding(
-//                 padding: EdgeInsets.all(8.0),
-//                 child: Text(
-//                   'Roll',
-//                   style: TextStyle(fontSize: 26),
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//       // floatingActionButton: FloatingActionButton(
-//       //   onPressed: () => Navigator.pop(context),
-//       //   child: const Icon(Icons.remove),
-//       // ),
-//     );
-//   }
-
-//   @override
-//   void dispose() {
-//     player.dispose();
-//     super.dispose();
-//   }
-// }
